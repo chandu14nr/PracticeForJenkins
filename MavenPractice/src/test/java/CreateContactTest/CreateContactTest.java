98e8782f6622e7e50f0b6dd4a4aa4bdd99b898a8
chandan <chandannr2000@gmail.com>
2024-09-04 15:55:29 -0400

    first commit

---
+++ MavenPractice/src/test/java/CreateContactTest/CreateContactTest.java
@@ -0,0 +1,28 @@
+package CreateContactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void CreateConTest()
+	{
+		System.out.println("CreateContactTest Executed");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+	
+	@Test
+	public void CreateConWithPhTest()
+	{
+		System.out.println("CreateContact with ph Test Executed");
+	}
+	
+
+}
---
+++ MavenPractice/src/test/java/CreateOrgTest/CreaateOrgTest.java
@@ -0,0 +1,19 @@
+package CreateOrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreaateOrgTest {
+
+	@Test
+	public void CreateOrTest()
+	{
+		System.out.println("CreateOrTest Executed");
+	}
+	
+	@Test
+	
+	public void CreateOrgWithDateTest()
+	{
+		System.out.println("CreateOrgWithDateTest Executed");
+	}
+}
