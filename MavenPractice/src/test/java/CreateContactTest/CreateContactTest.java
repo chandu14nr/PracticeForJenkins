af6b98c5a94ccfd8ada0ab12363a39f82125301c
chandan <chandannr2000@gmail.com>
2024-09-05 11:36:50 -0400

    added syso statements

--- MavenPractice/src/test/java/CreateContactTest/CreateContactTest.java
+++ MavenPractice/src/test/java/CreateContactTest/CreateContactTest.java
@@ -8,10 +8,12 @@ public class CreateContactTest {
 	public void CreateConTest()
 	{
 		System.out.println("CreateContactTest Executed");
+		String ENVIRONMENT=System.getProperty("Environment");
 		String URL=System.getProperty("url");
 		String BROWSER=System.getProperty("browser");
 		String USERNAME=System.getProperty("username");
 		String PASSWORD=System.getProperty("password");
+		System.out.println(ENVIRONMENT);
 		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(USERNAME);
--- MavenPractice/src/test/java/CreateOrgTest/CreaateOrgTest.java
+++ MavenPractice/src/test/java/CreateOrgTest/CreaateOrgTest.java
@@ -8,6 +8,15 @@ public class CreaateOrgTest {
 	public void CreateOrTest()
 	{
 		System.out.println("CreateOrTest Executed");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
 	}
 	
 	@Test
