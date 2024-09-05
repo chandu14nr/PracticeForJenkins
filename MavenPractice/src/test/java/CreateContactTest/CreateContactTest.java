e79e9dbfb450f3a45f07f1824245227cab5ead0f
chandan <chandannr2000@gmail.com>
2024-09-05 13:00:13 -0400

    added pro kabaddi

--- MavenPractice/src/test/java/CreateContactTest/CreateContactTest.java
+++ MavenPractice/src/test/java/CreateContactTest/CreateContactTest.java
@@ -1,5 +1,11 @@
 package CreateContactTest;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class CreateContactTest {
@@ -23,6 +29,23 @@ public class CreateContactTest {
 	@Test
 	public void CreateConWithPhTest()
 	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.prokabaddi.com/standings");
+		List<WebElement> pro = driver.findElements(By.xpath("//div[@class='table-head']/following-sibling::div[@class=\"table-body\"]/descendant::p[@class='name' ]"));
+		for (int i = 0; i <5; i++)
+		{
+			String text=pro.get(i).getText();
+			System.out.print(text+" ");
+			List<WebElement> wid = driver.findElements(By.xpath("//p[text()='"+text+"']/ancestor::div[@class='row-head']/div[@class='table-data form']//p"));
+			for (int j = 0; j <5; j++)
+			{
+				String text1=wid.get(j).getText();
+				System.out.print(text1);
+			}
+			System.out.println();
+		}
+		driver.quit();
+	
 		System.out.println("CreateContact with ph Test Executed");
 	}
 	
